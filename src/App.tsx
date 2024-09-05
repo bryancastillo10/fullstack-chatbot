e55fa5067f2e9152cbef50c32a80c6ed85f8db12
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2024-09-05 11:47:05 +0800

    rename folder components to landingpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import { Routes,Route } from "react-router-dom";
-import AppLayout from "./pages/AppLayout";
 import { Home, PageNotFound } from "./pages";
 
 const App = () => {
   return (
         <Routes>
-        <Route element={<AppLayout />}>
         <Route path="/" element={<Home />} />
-        </Route>
-
         <Route path="*" element={<PageNotFound />} />
       
       </Routes>
