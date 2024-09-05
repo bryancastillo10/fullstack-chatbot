bc2c18645f57147080b3ffce445bc3304a1d7653
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2024-09-05 16:03:36 +0800

    About Section

---
+++ src/landingpage/About/ImageGrid.tsx
@@ -0,0 +1,23 @@
+
+interface ImageGridProps{
+    images:(string | undefined)[];
+}
+
+const ImageGrid = ({images}:ImageGridProps) => {
+  return ( 
+    <section className="mt-16 p-10 items-stretch  
+    lg:mt-0 grid grid-cols-2 lg:grid-cols-3 gap-4 order-1 lg:order-2">
+        <div className="row-start-1 row-span-2 col-span-1 xl:col-span-2 ">
+            <img src={images[1]} alt={`${images[1]}-path`}  className="size-full  rounded-lg"/>
+        </div>
+        <div className="row-start-3 row-span-2 col-span-1 xl:col-span-2 ">
+            <img src={images[0]} alt={`${images[0]}-path`} className="size-full  rounded-lg" />
+        </div>
+        <div className="col-span-1 row-span-4">
+            <img src={images[2]} alt={`${images[2]}-path`} className="w-[80%] xl:w-fit lg:h-full "  />
+        </div>
+    </section>
+  )
+}
+
+export default ImageGrid;
--- src/landingpage/About/index.tsx
+++ src/landingpage/About/index.tsx
@@ -1,9 +1,10 @@
 import { useState,useEffect } from "react";
 import { SectionContainer } from "../../reusables";
 import { getImageUrl } from "../../config/api";
+import ImageGrid from "./ImageGrid";
 
 const About = () => {
-  
+  const [aboutImages,setAboutImages] = useState<(string | undefined)[]>([]);
   const bucket = "project-assets";
 
   const imageFilePaths = [
@@ -12,7 +13,7 @@ const About = () => {
     "about/WaterWorksImg.png"
   ];
 
-  const [aboutImages,setAboutImages] = useState<(string | undefined)[]>([]);
+  
 
   useEffect(()=> {
       const fetchAboutImages = async () => {
@@ -25,17 +26,28 @@ const About = () => {
   },[]);
 
   return (
-    <section className="mt-[-5px]  bg-secondary text-primary">
+    <section className="mt-[-5px] bg-secondary text-primary">
         <SectionContainer>
-        
-        <h1 className="py-10">Hello World</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, exercitationem?</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, laboriosam.</p>
-        <div className="flex items-center gap-4">
-          {aboutImages.map((img,index)=>(
-            <img key={index} src={img} alt={`${img}-path`}/>
-          ))}
-        </div>
+          <div className="grid grid-cols-1 xl:grid-cols-2 gap-10">
+            <div className=" xl:order-1 order-2">
+              <h1 className="font-comfortaa font-semibold mt-2 text-center lg:text-left">Our Story</h1>
+              <div className="mt-6 xl:mt-10 mb-6">
+                <p className="font-semibold tracking-wider text-[20px] xl:text-[25px] text-center lg:text-left">
+                  Providing clients with excellent consultation services in environmental compliances & 
+                  management for sustainable development
+                </p>
+              </div>
+              <div className="space-y-4 text-center lg:text-justify pt-1 pb-4">
+                <p className="indent-2 leading-normal">  Envirotech was first established in 2005 as a small machine parts store that supplied pipes, valves, and materials to industrial 
+                companies. In order to assure the quality and effectiveness of the products, the company's creative founders and fellow angel investors grew it into the design of 
+                water treatment technologies and employed specialists in this area. As time passes by, the company's peak business division—the consultation division—was established.</p>
+                <p className="indent-2 leading-normal">  Today, Envirotech is a leading environmental consulting firm, recognized for its <span className="text-accent">innovative solutions</span> in water and
+                 wastewater management, pollution control, and sustainable development. With a multidisciplinary team of engineers, scientists, and industry experts, we provide 
+                 comprehensive environmental services that help businesses comply with regulations, reduce their environmental footprint, and <span className="text-accent">achieve sustainability goals</span>.</p>
+              </div>
+            </div>
+            <ImageGrid images={aboutImages}/>
+          </div>       
         </SectionContainer>
     </section>
   )
