be5942028fde1b80d9c8b4fca7b8f6685c2fbef1
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2024-09-06 14:37:17 +0800

    ReusableSectionSubHeader

--- src/reusables/index.ts
+++ src/reusables/index.ts
@@ -1,5 +1,6 @@
 import Button from "./Button";
 import Linker from "./Linker";
 import SectionContainer from "./SectionContainer";
+import SectionSubHeader from "./SectionSubHeader";
 
-export {Button, Linker, SectionContainer};
+export {Button, Linker, SectionContainer, SectionSubHeader};
