af2b741b6e707295c8f979fe9a979714b13972f7
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2024-09-06 16:22:47 +0800

    InitialTechCardSetUp

--- src/landingpage/Technology/index.tsx
+++ src/landingpage/Technology/index.tsx
@@ -1,19 +1,39 @@
 import WaveDown from "../../assets/WaveDown";
 import { SectionContainer,SectionSubHeader } from "../../reusables";
-
+import { Wind, Share } from "phosphor-react";
 
 const Technology = () => {
   return (
     <section className="mt-[-5px]">
-        <div className="w-full">
+        <div className="w-full pb-14">
             <WaveDown/>
         </div>
         <SectionContainer>
-            <SectionSubHeader title="Technologies"/>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam vel excepturi a quia magni error 
-                maxime totam saepe dolore, atque beatae quaerat corrupti illo, doloribus numquam tempora, quis cupiditate! 
-                Cumque itaque, nisi iusto iure ipsum tempore 
-                assumenda, quidem ad minima saepe cum sunt libero culpa commodi asperiores. At, ducimus numquam?</p>
+            <SectionSubHeader title="Technologies" caption="Get to know the latest trends in the industry"/>
+            
+            {/* Individual Card */}
+            <div className="w-[240px] h-[360px] rounded-2xl border border-secondary  mb-4">
+              <div className="px-5 pt-[30px]">
+                <img src="https://placehold.co/200x150" alt="sample-card-pic" />
+              </div>
+              <article className="m-5">
+                <div className="flex items-center mb-2 gap-1">
+                  <Wind size="20"/>
+                  <h1 className="font-semibold">Technology Name</h1>
+                </div>
+                <div className="bg-secondary px-3 py-1 rounded-3xl w-fit">
+                  <h1 className="font-light text-primary text-sm">Envi. Category</h1>
+                </div>
+                <div className="mt-2 text-xs ">
+                  <p className="text-balance line-clamp-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Cumque ea doloribus error dolor illum nihil, pariatur commodi voluptas atque tenetur quos cupiditate quod possimus 
+                    ipsa ipsam voluptatem dolorem iste illo?
+                  </p>
+                  <p className="mt-3 mb-6 flex items-center gap-2">Read More <span><Share size="14"/></span></p>
+                </div>
+
+              </article>
+            </div>
         </SectionContainer>
     </section>
   )
