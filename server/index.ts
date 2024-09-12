9c070100ea416873d7fcde07e2aa0aa8bebdf599
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2024-09-12 16:02:04 +0800

    Set Up Local Postgres by Prisma

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,23 @@
 import express from 'express';
-const app = express()
+import { PrismaClient } from '@prisma/client';
+
+const app = express();
+const prisma = new PrismaClient();
 
 const PORT = 3000;
 
 app.get("/", (req,res)=> {
-    res.send("Node TS is Ready");
+    res.send("EnviroTech Server is Ready");
 });
 
-app.listen(PORT, ()=> {
-    console.log(`App Server is listening on port ${PORT}`);
+app.listen(PORT, async ()=> {
+    try{
+        await prisma.$connect();
+        console.log('Connected to PostgreSQL Database by Prisma ORM');
+    }
+    catch(error){
+        console.error('Failed to connect to PostgreSQL',error.message);
+        process.exit(1);
+    }
+    console.log(`Server is listening on port ${PORT}`)
 });
