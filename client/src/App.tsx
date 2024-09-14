c439371cfe2457f1b829007895dc496bf82ba8df
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2024-09-14 08:01:55 +0800

    AppLayout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,21 @@
 import { Routes,Route } from "react-router-dom";
-import { Home, PageNotFound, SignIn, SignUp, TechPage } from "./pages";
+import { LandingPage, 
+          AppLayout,
+          PageNotFound, 
+          SignIn, SignUp, 
+          TechPage } from "./pages";
 
 const App = () => {
   return (
         <Routes>
-        <Route path="/" element={<Home />}/>
+        <Route path="/" element={<LandingPage />}/>
         <Route path="/signin" element={<SignIn/>}/>
         <Route path="/signup" element={<SignUp/>}/>
         <Route path="/tech" element={<TechPage/>}>
             <Route path=":id" />
+        </Route>
+        <Route path="user" element={<AppLayout/>}>
+            <Route path="home" index element={<p>Home</p>}/>
         </Route>
         <Route path="*" element={<PageNotFound />} />     
       </Routes>
