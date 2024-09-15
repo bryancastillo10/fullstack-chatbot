68701086797320e4b3c86ee0966e1b14603eebd4
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2024-09-15 22:34:25 +0800

    SignUpLayout

---
+++ client/src/ui/AuthNavbar.tsx
@@ -0,0 +1,23 @@
+import NavLogo from "/earth.png";
+import { ArrowLeft } from "@phosphor-icons/react";
+import { Link } from "react-router-dom";
+
+
+const AuthNavbar = () => {
+  return (
+    <nav className="p-4 border border-black sticky top-0 bg-primary text-black z-20"
+    >
+       <div className='flex justify-between xl:px-4 items-center gap-10'>
+          <Link to="/" className="flex flex-col items-center text-hover-link">
+            <ArrowLeft size={20}/> <span className='text-xs ml-2'> Back to Home</span>
+          </Link>
+          <div className="flex items-center gap-1">
+            <img src={NavLogo} alt="EnviroTech-logo" className="size-10" />
+            <h1 className="font-semibold font-quicksand">EnviroTech</h1>
+          </div>
+        </div>
+    </nav>
+  )
+}
+
+export default AuthNavbar
