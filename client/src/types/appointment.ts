978f514be8b1e80119aa20f74ce6402d99dc574d
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2024-10-01 17:09:49 +0800

    RTK Query Appointment

--- client/src/types/appointment.ts
+++ client/src/types/appointment.ts
@@ -3,7 +3,7 @@ export interface GetServiceResponse {
     name:string;
 }
 
-interface ServiceProps{
+type ServiceProps = {
     service_id:string;
     name:string;
     description:string;
@@ -20,3 +20,40 @@ export interface GetConsultantsResponse {
     rating:number;
     services?:ServiceProps
 }
+
+type AppointmentDataProps = {
+    appointment_id:string;
+    user_id:string;
+    consultant_id:string;
+    service_id:string;
+    topic:string;
+    message:string;
+    createdAt:Date;
+    startDate:Date;
+    endDate:Date;
+    appointmentTime:string;
+    status:string;
+}
+export interface AppointmentRequest{
+    user_id:string;
+    consultant_id:string;
+    service_id:string;
+    topic:string;
+    message:string;
+    startDate:Date;
+    endDate:Date;
+    appointmentTime:string;
+}
+
+export interface CreateGetAppointment extends AppointmentDataProps{
+    consultant?: { name: string; }
+    service?: {name:string }
+} 
+
+export interface UpdateDeleteAppointment<T> {
+    message:string;
+    updatedAppointment: T;
+}
+
+export type UpdateAppointment = UpdateDeleteAppointment<AppointmentDataProps>;
+export type DeleteAppointment = UpdateDeleteAppointment<AppointmentDataProps>;
