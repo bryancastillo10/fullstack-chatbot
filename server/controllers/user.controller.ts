85deda9379b996c2c9e40007de5e6109cf79e7aa
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2024-10-07 13:13:36 +0800

    revise schema for cascade delete

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -38,8 +38,23 @@ export const deleteProfile = async (req: Request, res: Response) => {
   const { user_id } = req.params;
 
   try {
-    await prisma.user.delete({
+    // Appointment Schema
+    await prisma.appointment.deleteMany({
       where: { user_id },
+    });
+
+    // NewsFeed Schema
+    await prisma.newsFeed.deleteMany({
+      where: { user_id },
+    });
+
+    const deletedProfile = await prisma.user.delete({
+      where: { user_id },
+    });
+
+    res.status(200).json({
+      message: "User account has been successfully deleted",
+      deletedProfile,
     });
   } catch (error) {
     console.error("Error at the deleteProfile controller", error.message);
