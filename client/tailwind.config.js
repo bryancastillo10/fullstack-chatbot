28c0cd419ed70d33e824fe0710584908d22faed3
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2024-10-23 19:57:16 +0800

    rebuild project

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,30 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors:{
-        white:"#F4F3F2",
-        black:"#403F4C",
-        primary:"#F9EEE7",
-        secondary:"#0E8173",
-        accent:"#E9D417",
-        cream:"#E4D7CF"
-      },
-      fontFamily:{
-        quicksand:["Quicksand","sans-serif"],
-        comfortaa:["Comfortaa","sans-serif"]
-      }
-    },
-    fontWeight: {
-      base: '400',
-      semibold: '600',
-      bold:'800'
-    },
+    extend: {},
   },
   plugins: [],
-}
-
+};
--- server/routes/consultant.routes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getConsultants } from "../controllers/consultant.controller";
-
-const router = express.Router();
-
-// Endpoints
-router.get("/view", getConsultants)
-
-export default router;
