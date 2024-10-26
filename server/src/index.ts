b2b71efc7dd9fcd6eae58d1331662d12a9581abe
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2024-10-26 22:25:33 +0800

    json scripts node

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,10 @@
 import express from "express";
+import { Request, Response } from "express";
 
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello Node TS");
 });
 
