8c5bfe7d3f88fa790d113853824df0543eda60ff
bryancastillo10 <bryanangelo.castillo.10@gmail.com>
2024-12-06 15:08:58 +0800

    🧩 database config

--- src/data/interface.ts
+++ src/data/interface.ts
@@ -1,4 +1,3 @@
-
 export interface ColorTypes<T>  {
     id: T;
     colorHeader: T;
@@ -12,9 +11,17 @@ export interface CoordTypes  {
 }
 
 
-export interface INotesData{
-    $id: number;
+export interface INotesData {
+    $id: number | string;
     body: string;
     colors: string;
     position: string;
+}
+
+export interface INotesRepository {
+    create: (payload: INotesData, id?: string) => Promise<INotesData>;
+    get: (id: string) => Promise<INotesData>;
+    update: (id: string, payload: Partial<INotesData>) => Promise<INotesData>;
+    delete: (id: string) => Promise<void>;
+    list: (queries: string[]) => Promise<INotesData[]>;
 }
