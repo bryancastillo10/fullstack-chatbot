e55d3277bea4fb481544cb9d87b60b825a5b31d7
bryancastillo10 <bryanangelo.castillo.10@gmail.com>
2024-12-06 08:34:42 +0800

    remove prev project

--- client/src/types/modal.ts
+++
@@ -1,7 +0,0 @@
-import { CreateGetAppointment } from "./appointment";
-
-export interface AppointmentModalProps {
-  selectedAppointment: CreateGetAppointment;
-  handleCloseModal: () => void;
-  onAppointmentChange: () => void;
-}
--- server/utils/supabaseClient.ts
+++
@@ -1,8 +0,0 @@
-import { createClient, SupabaseClient } from "@supabase/supabase-js";
-
-const supabaseUrl = process.env.SUPABASE_URL as string;
-const supabaseKey = process.env.SUPABASE_ANON_KEY as string;
-
-const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
